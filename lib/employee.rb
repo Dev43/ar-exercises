68f297ccde17ca9113578606f240fefa3b4ef5b2
Patrick Guay <patrick.guay43@gmail.com>
2017-02-14 23:12:56 +0000

    made the store undeletable if more than 0 employees;

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,19 @@
 class Employee < ActiveRecord::Base
-belongs_to :store
-validates :store, presence: true
-validates :first_name, presence: true
-validates :last_name, presence: true
-validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200}
+  belongs_to :store
+
+  validates :store, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200}
+
+  before_create :random_password
+
+  private
+  def random_password
+    random_psw = (0...8).map { (65 + rand(26)).chr }.join
+    self.password = random_psw
+  end
+
+
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,14 +1,29 @@
 class Store < ActiveRecord::Base
-has_many :employees
-validates :name, length: {minimum: 3}
-validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-validate :carry_men_or_women
+  has_many :employees
+
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
+  validate :carry_men_or_women
+
+  before_destroy :valid_destroy?
 
 
+
+private
   def carry_men_or_women
     if !(mens_apparel || womens_apparel)
       errors.add(:mens_apparel, "Needs to at least serve men's or women's wear")
       errors.add(:womens_apparel, "Needs to at least serve men's or women's wear")
+    end
+  end
+
+  def valid_destroy?
+    if(self.employees.count > 0)
+      errors.add(:employees, "Cannot destroy Database, there is at least 1 employee!")
+      throw :abort
+    else
+      true
     end
   end
 end
