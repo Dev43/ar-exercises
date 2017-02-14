09c357fcf5fb2d00a78d4ea263db6b50b845449c
Patrick Guay <patrick.guay43@gmail.com>
2017-02-14 21:44:44 +0000

    end of ex 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Pat", last_name: "Guay", hourly_rate: 50)
+@store1.employees.create(first_name: "Hilke", last_name: "Gune", hourly_rate: 200)
+
+@store2.employees.create(first_name: "Rick", last_name: "frefe", hourly_rate: 60)
+@store2.employees.create(first_name: "Patroeick", last_name: "fewfwefew", hourly_rate: 122)
+@store2.employees.create(first_name: "Rage", last_name: "Gunther", hourly_rate: 2000)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,12 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
+puts "Give me a store name please"
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+puts new_store.errors.messages
+ new_store.errors.messages.each do |message|
+  puts message
+end
 # Your code goes here ...
